aa1acdac515facfc23526bcdd91a8b56c0e54639
anbehindY <yanminthwin@gmail.com>
2023-08-22 16:52:36 +0630

    Setup rack cors

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,6 @@ module FurnitureMallBackend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -35,5 +34,13 @@ module FurnitureMallBackend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+          headers: :any,
+          methods: [:get, :post, :options]
+      end
+    end
   end
 end
