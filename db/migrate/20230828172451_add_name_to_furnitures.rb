d167149699064186d92117ff3c08de7c3e13c0d5
prius2055 <prius2055@gmail.com>
2023-08-28 18:32:31 +0100

    create api for furnitures and added name column to furniture table

---
+++ db/migrate/20230828172451_add_name_to_furnitures.rb
@@ -0,0 +1,5 @@
+class AddNameToFurnitures < ActiveRecord::Migration[7.0]
+  def change
+    add_column :furnitures, :name, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_25_145023) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_28_172451) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,6 +31,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_25_145023) do
     t.integer "warranty"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["user_id"], name: "index_furnitures_on_user_id"
   end
 
