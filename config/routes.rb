cbb7a6fcddfa1fbb7572ec3ca09efb245cdb76a2
Esther Udondian <eudondian@gmail.com>
2023-08-29 08:04:18 +0100

    Update route.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :furnitures, only: [:show]
+      resources :furnitures, only: [:show,  :index]
     end
 
   end
