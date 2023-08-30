bc172435d46781c3b03ed22cfd3e2916ae2488c1
anbehindY <yanminthwin@gmail.com>
2023-08-30 13:30:46 +0630

    Implement appointment function

---
+++ app/controllers/api/v1/appointments_controller.rb
@@ -0,0 +1,29 @@
+class API::V1::AppointmentsController < ApplicationController
+  # For 'My Appointments' page
+  def index
+    @appointments = current_user.appointments
+    render json: @appointments
+  end
+
+  # For 'Make Appointment' page
+  def new
+    @appointment = Appointment.new
+  end
+
+  # Form action for 'Make Appointment' page
+  def create
+    @furniture = Furniture.find(params[:furniture_id])
+    @appointment = Appointment.create(current_user.id, @furniture.id, appointment_params)
+    if @appointment.save
+      render json: @appointment
+    else
+      render json: { error: 'Unable to create appointment.' }, status: unprocessable_entity
+    end
+  end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:appoint_date)
+  end
+end
