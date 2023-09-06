ded768060ae3ea8ff9bc44165f1adda32015666a
prius2055 <prius2055@gmail.com>
2023-09-06 22:47:31 +0100

    Fix linters

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::AppointmentsController < ApplicationController
-   before_action :authenticate_user!
+  before_action :authenticate_user!
   def index
     @appointments = Appointment.all
     render json: @appointments
