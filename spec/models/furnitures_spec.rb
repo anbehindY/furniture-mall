a43494783527d4ae2e1cb249c9e97160e3ce6438
anbehindY <yanminthwin@gmail.com>
2023-09-08 03:08:40 +0630

    Fix furniture model spec

--- spec/models/furnitures_spec.rb
+++ spec/models/furnitures_spec.rb
@@ -6,7 +6,21 @@ RSpec.describe 'furnitures', type: :model do
   let(:sofa) { Furniture.create!(name: 'sofa', description: 'a sofa', price: 100, user_id: user1.id) }
   let(:appointment1) { Appointment.create!(appoint_date: '2021-01-01', user_id: user1.id, furniture_id: sofa.id) }
   let(:appointment2) { Appointment.create!(appoint_date: '2021-01-01', user_id: user2.id, furniture_id: sofa.id) }
+  
+  context 'associations' do
+    it 'has many appointments' do
+      expect(sofa.appointments).to include(appointment1)
+      expect(sofa.appointments).to include(appointment2)
+    end
+
+    it 'has many users through appointments' do
+      expect(appointment1.user).to eq(user1)
+      expect(appointment2.user).to eq(user2)
+    end
+  end
+
   context 'validations' do
+    
     it 'is valid with valid attributes' do
       expect(sofa).to be_valid
     end
@@ -24,17 +38,6 @@ RSpec.describe 'furnitures', type: :model do
     it 'is not valid without a password' do
       sofa.price = nil
       expect(sofa).to_not be_valid
-    end
-  end
-
-  context 'associations' do
-    it 'belongs to user' do
-      expect(sofa.user).to eq(user1)
-    end
-
-    it 'has many appointments' do
-      expect(sofa.appointments).to include(appointment1)
-      expect(sofa.appointments).to include(appointment2)
     end
   end
 end
