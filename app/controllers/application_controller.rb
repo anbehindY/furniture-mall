dd0556445d0a22e5948bef087c8e2c6611efa758
prius2055 <prius2055@gmail.com>
2023-09-07 23:29:18 +0100

    solve issue from review request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::API
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email username])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[email username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email username role])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[email username role])
   end
 end
