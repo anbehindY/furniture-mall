42204650b618096cf49cd0b8b096057e587d8c89
prius2055 <prius2055@gmail.com>
2023-09-08 18:16:27 +0100

    update furniture model

--- app/models/furniture.rb
+++ app/models/furniture.rb
@@ -1,5 +1,5 @@
 class Furniture < ApplicationRecord
-  has_many :appointments
+  has_many :appointments, foreign_key: 'furniture_id', dependent: :destroy
   has_many :users, through: :appointments
   validates :name, :description, :price, presence: true
 end
